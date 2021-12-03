752fced321cdf23ddd28e44da8b80a71f6125fdf
Zsvob <svobodazachary@gmail.com>
2021-12-03 13:12:46 -0500

    send password to html and fix no seletors

--- Develop/script.js
+++ Develop/script.js
@@ -1,57 +1,61 @@
- // Assignment code here
- var lcaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
- var ucaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
- var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
- var specialChar = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', ]
- var zeroArray = []
 
+var lcaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var ucaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var specialChar = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+',]
+var zeroArray = []
 
+//called after button is clicked.
+generatePassword = function () {
 
- // Password length selection function
- var characterQuantity = function(){
+  // Password length selection function
+  var characterQuantity = function () {
     var howmany = window.prompt("How many Characters would you like? 8 through 128")
-    if (8 < howmany < 128) {
+    {
       value = parseInt(howmany)
-      return value;
-    } else { 
-      characterQuantity
+      if (8 < value < 128) {
+        return value;
+      } else {
+        characterQuantity();
+      }
     }
   }
-
-// Character selectors. takes User Input and selects appropriate global array (lowercase letters, uppercase letters, numbers, and special characters)
+  
+  // Character selectors. takes User Input and selects appropriate global array (lowercase letters, uppercase letters, numbers, and special characters)
   var lCLL = function () {
-    var lowlet = window.confirm("Would you like lower case Letters?")
+    var lowlet = window.confirm("Would you like lower case Letters? Okay for YES, Cancel for NO")
     if (lowlet) {
       var value = lcaseLetters
     }
-    else {var value = zeroArray}
+    else { var value = zeroArray }
     return value;
   }
   var uCLL = function () {
-    var uplet = window.confirm("Would you like uppercase Letters?")
+    var uplet = window.confirm("Would you like uppercase Letters?  Okay for YES, Cancel for NO")
     if (uplet) {
       var value = ucaseLetters
     }
-    else {var value = zeroArray}
+    else { var value = zeroArray }
     return value;
   }
   var num = function () {
-    var nums = window.confirm("Would you like numbers?")
+    var nums = window.confirm("Would you like numbers? Okay for YES, Cancel for NO")
     if (nums) {
       var value = numbers
     }
-    else {var value = zeroArray}
+    else { var value = zeroArray }
     return value;
   }
   var specChar = function () {
-    var spec = window.confirm("Would you like lower Owasp approved special characters?")
+    var spec = window.confirm("Would you like lower Owasp approved special characters?  Okay for YES, Cancel for NO")
     if (spec) {
       var value = specialChar
     }
-    else {var value = zeroArray}
+    else { var value = zeroArray }
     return value;
   }
- 
+
+  // Variables for password gen logic
   var charQty = characterQuantity();
   var array1 = lCLL();
   var array2 = uCLL();
@@ -60,24 +64,27 @@
   // Combine selected Arrays into a masterArray for character selection
   const masterArray = [...array1, ...array2, ...array3, ...array4]
 
-// console logs for testing purposes to be removed
-console.log(masterArray)
-console.log(charQty) 
-//console.log(masterArray[Math.floor(Math.random()* masterArray.length)])
-
-var password = []
-
-for (var i = 0; i < charQty; i++) {
-  var passW = masterArray[Math.floor(Math.random()* masterArray.length)];
-  password.push(passW);
+  // function to stop no selectors entry
+  var runPass = function () {
+    if (masterArray.length === 0) {
+      var noArray = window.alert("CAN NOT RUN PASSWORD GENERATOR! Please select at least one type of characters.  Try again.")
+      generatePassword();
+    } else {
+      // Logic for Password creation
+      var passwrd = []
+      for (var i = 0; i < charQty; i++) {
+        var passW = masterArray[Math.floor(Math.random() * masterArray.length)];
+        passwrd.push(passW);
+      }
+      return passwrd.join('');
+    }
+    
   }
+  var x = runPass();
+  return x;
+  
+}
 
-console.log(password.join(''))
-
-
-
-
- //var generatePassword = function () {}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -87,7 +94,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password
 
 }
 
