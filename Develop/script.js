ef5bf3874995d8f7ef18a40568e49fc5786e7f18
Zsvob <svobodazachary@gmail.com>
2021-12-03 10:43:49 -0500

    start password length logic

--- Develop/script.js
+++ Develop/script.js
@@ -3,14 +3,26 @@
  var ucaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
  var specialChar = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', ]
- var fixer = []
+ var zeroArray = []
+
  
+ var characterQuantity = function(){
+    var howmany = window.prompt("How many Characters would you like? 8 through 128")
+    if (8 < howmany < 128) {
+      value = parseInt(howmany)
+      return value;
+    } else { 
+      characterQuantity
+    }
+  }
+
+
   var lCLL = function () {
     var lowlet = window.confirm("Would you like lower case Letters?")
     if (lowlet) {
       var value = lcaseLetters
     }
-    else {var value = fixer}
+    else {var value = zeroArray}
     return value;
   }
   var uCLL = function () {
@@ -18,7 +30,7 @@
     if (uplet) {
       var value = ucaseLetters
     }
-    else {var value = fixer}
+    else {var value = zeroArray}
     return value;
   }
   var num = function () {
@@ -26,7 +38,7 @@
     if (nums) {
       var value = numbers
     }
-    else {var value = fixer}
+    else {var value = zeroArray}
     return value;
   }
   var specChar = function () {
@@ -34,27 +46,23 @@
     if (spec) {
       var value = specialChar
     }
-    else {var value = fixer}
+    else {var value = zeroArray}
     return value;
   }
-
+ 
+  var charQty = characterQuantity();
   var array1 = lCLL();
   var array2 = uCLL();
   var array3 = num();
   var array4 = specChar();
+  const masterArray = [...array1, ...array2, ...array3, ...array4]
 
-  console.log(array1)
-  console.log(array2)
-  console.log(array3)
-  console.log(array4)
-  
-const masterArray = [...array1, ...array2, ...array3, ...array4] 
+
 console.log(masterArray)
-  //console.log(Math.floor(Math.random()* arraySelectorLength))
+console.log(charQty) 
+
+console.log(masterArray[Math.floor(Math.random()* masterArray.length)])
   
-
-
- // var characterQuantity = window.prompt("How many Characters would you like? 8 through 128")
 
 
  //var generatePassword = function () {}
